d228145be0b34605273bc2f012d87c1964ea9623
MarceloJoel <marcelorodriguez0369@gmail.com>
2024-12-20 09:50:03 -0500

    Se agrega mensaje en el main

--- src/main/java/com/aynisac/proyectoayni/ProyectoAyni.java
+++ src/main/java/com/aynisac/proyectoayni/ProyectoAyni.java
@@ -6,6 +6,7 @@ import com.aynisac.proyectoayni.igu.Principal;
 public class ProyectoAyni {
     
     public static void main(String[] args) {
+        System.out.println("Modificacion en el main");
         Principal princ = new Principal();
         princ.setVisible(true);
         princ.setLocationRelativeTo(null);
