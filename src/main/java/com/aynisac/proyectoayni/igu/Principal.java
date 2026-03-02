39f2fb508df0ad6a6e0044082780197af3050d2f
MarceloJoel <marcelorodriguez0369@gmail.com>
2026-03-02 13:12:15 -0500

    fix: se areglo el frame Principal

--- src/main/java/com/aynisac/proyectoayni/igu/Principal.java
+++ src/main/java/com/aynisac/proyectoayni/igu/Principal.java
@@ -1,83 +1,134 @@
 package com.aynisac.proyectoayni.igu;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import javax.swing.JFrame;
 
 public class Principal extends javax.swing.JFrame {
-
+    
     public Principal() {
         initComponents();
         setLocationRelativeTo(null);
+        LocalDate fechaActual = LocalDate.now();
+        jLabel3.setText("Fecha: " + fechaActual.format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
     }
+    
+    
 
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        desktopPane = new javax.swing.JDesktopPane();
-        jLabel1 = new javax.swing.JLabel();
+        jDesktopPane1 = new javax.swing.JDesktopPane();
         jLabel2 = new javax.swing.JLabel();
-        jMenuBar1 = new javax.swing.JMenuBar();
-        jMenu1 = new javax.swing.JMenu();
+        btnViabilidad = new javax.swing.JButton();
+        btnPresupuesto = new javax.swing.JButton();
+        btnOC = new javax.swing.JButton();
+        jPanel1 = new javax.swing.JPanel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel1 = new javax.swing.JLabel();
+        jMenuBar2 = new javax.swing.JMenuBar();
+        jMenu2 = new javax.swing.JMenu();
         mniViabilidad = new javax.swing.JMenuItem();
         mniPresupuesto = new javax.swing.JMenuItem();
-        mniOC = new javax.swing.JMenuItem();
+        mnuOC = new javax.swing.JMenuItem();
+        jSeparator1 = new javax.swing.JPopupMenu.Separator();
         mniExit = new javax.swing.JMenuItem();
+        jMenu3 = new javax.swing.JMenu();
+        jMenu4 = new javax.swing.JMenu();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Ayni SAC");
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        jLabel1.setIcon(new javax.swing.ImageIcon("C:\\NetBeansProject\\ProyectoAyni\\src\\main\\java\\com\\aynisac\\proyectoayni\\icono\\portada.jpg")); // NOI18N
-        desktopPane.add(jLabel1);
-        jLabel1.setBounds(0, 0, 800, 520);
+        jDesktopPane1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        jLabel2.setIcon(new javax.swing.ImageIcon("C:\\NetBeansProject\\ProyectoAyni\\src\\main\\java\\com\\aynisac\\proyectoayni\\icono\\logoAyni_horizontal.png")); // NOI18N
-        desktopPane.add(jLabel2);
-        jLabel2.setBounds(0, 510, 800, 120);
+        jLabel2.setIcon(new javax.swing.ImageIcon("D:\\ProyectoAyni\\src\\main\\java\\com\\aynisac\\proyectoayni\\imagenes\\ayni_sac_logo_cuadrado.jpg")); // NOI18N
+        jDesktopPane1.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(300, 60, 190, 190));
 
-        getContentPane().add(desktopPane, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 800, 650));
+        btnViabilidad.setText("Nuevo Pedido");
+        btnViabilidad.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnViabilidadActionPerformed(evt);
+            }
+        });
+        jDesktopPane1.add(btnViabilidad, new org.netbeans.lib.awtextra.AbsoluteConstraints(310, 280, 160, 50));
 
-        jMenu1.setIcon(new javax.swing.ImageIcon("C:\\NetBeansProject\\recursos\\BotonMenu.png")); // NOI18N
+        btnPresupuesto.setText("Ver Presupuestos");
+        btnPresupuesto.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnPresupuestoActionPerformed(evt);
+            }
+        });
+        jDesktopPane1.add(btnPresupuesto, new org.netbeans.lib.awtextra.AbsoluteConstraints(310, 350, 160, 50));
 
-        mniViabilidad.setIcon(new javax.swing.ImageIcon("C:\\NetBeansProject\\recursos\\plus.png")); // NOI18N
+        btnOC.setText("Estado de OC");
+        btnOC.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnOCActionPerformed(evt);
+            }
+        });
+        jDesktopPane1.add(btnOC, new org.netbeans.lib.awtextra.AbsoluteConstraints(310, 420, 160, 50));
+
+        jLabel3.setText("jLabel3");
+        jPanel1.add(jLabel3);
+
+        jDesktopPane1.add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(650, 610, 140, 30));
+
+        jLabel1.setIcon(new javax.swing.ImageIcon("D:\\ProyectoAyni\\src\\main\\java\\com\\aynisac\\proyectoayni\\imagenes\\uno.jpg")); // NOI18N
+        jDesktopPane1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 800, 650));
+
+        getContentPane().add(jDesktopPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 800, 650));
+
+        jMenu2.setText("Menú");
+        jMenu2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenu2ActionPerformed(evt);
+            }
+        });
+
         mniViabilidad.setText("Viabilidad del pedido");
         mniViabilidad.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 mniViabilidadActionPerformed(evt);
             }
         });
-        jMenu1.add(mniViabilidad);
+        jMenu2.add(mniViabilidad);
 
-        mniPresupuesto.setIcon(new javax.swing.ImageIcon("C:\\NetBeansProject\\recursos\\plus.png")); // NOI18N
         mniPresupuesto.setText("Revisar y aceptar presupuesto");
         mniPresupuesto.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 mniPresupuestoActionPerformed(evt);
             }
         });
-        jMenu1.add(mniPresupuesto);
+        jMenu2.add(mniPresupuesto);
 
-        mniOC.setIcon(new javax.swing.ImageIcon("C:\\NetBeansProject\\recursos\\plus.png")); // NOI18N
-        mniOC.setText("Cumplimiento de la Orden de Compra");
-        mniOC.addActionListener(new java.awt.event.ActionListener() {
+        mnuOC.setText("Cumplimiento de la Orden de Compra");
+        mnuOC.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                mniOCActionPerformed(evt);
+                mnuOCActionPerformed(evt);
             }
         });
-        jMenu1.add(mniOC);
+        jMenu2.add(mnuOC);
+        jMenu2.add(jSeparator1);
 
-        mniExit.setIcon(new javax.swing.ImageIcon("C:\\NetBeansProject\\recursos\\plus.png")); // NOI18N
         mniExit.setText("Exit");
         mniExit.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 mniExitActionPerformed(evt);
             }
         });
-        jMenu1.add(mniExit);
+        jMenu2.add(mniExit);
 
-        jMenuBar1.add(jMenu1);
+        jMenuBar2.add(jMenu2);
 
-        setJMenuBar(jMenuBar1);
+        jMenu3.setText("Edit");
+        jMenuBar2.add(jMenu3);
+
+        jMenu4.setText("Help");
+        jMenuBar2.add(jMenu4);
+
+        setJMenuBar(jMenuBar2);
 
         getAccessibleContext().setAccessibleDescription("");
 
@@ -91,6 +142,10 @@ public class Principal extends javax.swing.JFrame {
         frmViabilidad.setLocationRelativeTo(null);
     }//GEN-LAST:event_mniViabilidadActionPerformed
 
+    private void jMenu2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenu2ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jMenu2ActionPerformed
+
     private void mniPresupuestoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_mniPresupuestoActionPerformed
         RevisarAceptarPresupuesto frmPresupuesto = new RevisarAceptarPresupuesto();
         frmPresupuesto.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
@@ -98,28 +153,56 @@ public class Principal extends javax.swing.JFrame {
         frmPresupuesto.setLocationRelativeTo(null);
     }//GEN-LAST:event_mniPresupuestoActionPerformed
 
-    private void mniOCActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_mniOCActionPerformed
+    private void mnuOCActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_mnuOCActionPerformed
         CumplimientoOC frmCumplimientoOC = new CumplimientoOC();
         frmCumplimientoOC.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         frmCumplimientoOC.setVisible(true);
         frmCumplimientoOC.setLocationRelativeTo(null);
-    }//GEN-LAST:event_mniOCActionPerformed
+    }//GEN-LAST:event_mnuOCActionPerformed
 
     private void mniExitActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_mniExitActionPerformed
         System.exit(0);
     }//GEN-LAST:event_mniExitActionPerformed
 
+    private void btnViabilidadActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnViabilidadActionPerformed
+        ViabilidadPedido frmViabilidad = new ViabilidadPedido();
+        frmViabilidad.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frmViabilidad.setVisible(true);
+        frmViabilidad.setLocationRelativeTo(null);
+    }//GEN-LAST:event_btnViabilidadActionPerformed
+
+    private void btnPresupuestoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnPresupuestoActionPerformed
+        RevisarAceptarPresupuesto frmPresupuesto = new RevisarAceptarPresupuesto();
+        frmPresupuesto.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frmPresupuesto.setVisible(true);
+        frmPresupuesto.setLocationRelativeTo(null);
+    }//GEN-LAST:event_btnPresupuestoActionPerformed
+
+    private void btnOCActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnOCActionPerformed
+        CumplimientoOC frmCumplimientoOC = new CumplimientoOC();
+        frmCumplimientoOC.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frmCumplimientoOC.setVisible(true);
+        frmCumplimientoOC.setLocationRelativeTo(null);
+    }//GEN-LAST:event_btnOCActionPerformed
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JDesktopPane desktopPane;
+    private javax.swing.JButton btnOC;
+    private javax.swing.JButton btnPresupuesto;
+    private javax.swing.JButton btnViabilidad;
+    private javax.swing.JDesktopPane jDesktopPane1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
-    private javax.swing.JMenu jMenu1;
-    private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JMenu jMenu2;
+    private javax.swing.JMenu jMenu3;
+    private javax.swing.JMenu jMenu4;
+    private javax.swing.JMenuBar jMenuBar2;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPopupMenu.Separator jSeparator1;
     private javax.swing.JMenuItem mniExit;
-    private javax.swing.JMenuItem mniOC;
     private javax.swing.JMenuItem mniPresupuesto;
     private javax.swing.JMenuItem mniViabilidad;
+    private javax.swing.JMenuItem mnuOC;
     // End of variables declaration//GEN-END:variables
-
 }
